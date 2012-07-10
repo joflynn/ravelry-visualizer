cf1a270a52980179bf9c5a7bc60f7d5e95114dc6
Joe Flynn <joflynn@kilgore.(none)>
2012-07-10 01:13:12 -0400

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RavelryVisualizer::Application.config.secret_token = '0586856f4b73d480adfb9aaab78d2d36cdcff5e2d874ff3616f5a046be0de1ffdf7f4824e32d127bdf10165c2a60907fb5ee3295a50494ca39b107d4184c6ca7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+RavelryVisualizer::Application.config.session_store :cookie_store, :key => '_ravelry_visualizer_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# RavelryVisualizer::Application.config.session_store :active_record_store
